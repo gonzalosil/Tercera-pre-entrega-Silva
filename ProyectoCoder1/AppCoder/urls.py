449a97feb91242166048b8cb8e5dc0b0c710b690
Gonzalo Silva <30927470+gonzalosil@users.noreply.github.com>
2024-07-28 20:50:41 -0300

    Terminado, falta ponerle para buscar profesores alumnos y entregables y tambien crearlos

--- ProyectoCoder1/AppCoder/urls.py
+++ ProyectoCoder1/AppCoder/urls.py
@@ -4,9 +4,12 @@ from AppCoder import views
 
 urlpatterns = [
     path('', views.inicio),
+    path('inicio', views.inicio),
     path('cursos', views.cursos, name="Cursos"),
     path('profesores', views.profesores),
     path('estudiantes', views.estudiantes),
     path('entregables', views.entregables),
-
+    path('cursoFormulario', views.cursoFormulario),
+    path('busquedaCamada', views.busquedaCamada),
+    path('buscar/', views.buscar),
 ]
--- ProyectoCoder1/AppCoder/views.py
+++ ProyectoCoder1/AppCoder/views.py
@@ -1,32 +1,65 @@
 from django.shortcuts import render
 from AppCoder.models import Curso
 from django.http import HttpResponse
+from AppCoder.forms import CursoFormulario
 # Create your views here.
 
 
 def inicio(request):
 
-    return HttpResponse('Vista inicio')
+    return render(request, "AppCoder/inicio.html")
 
 
 def cursos(request):
 
-    return HttpResponse('Vista cursos')
+    return render(request, "AppCoder/cursos.html")
 
 
 def profesores(request):
 
-    return HttpResponse('vista profesores')
+    return render(request, "AppCoder/profesores.html")
 
 
 def estudiantes(request):
 
-    return HttpResponse('Vista estudiantes')
+    return render(request, "AppCoder/estudiantes.html")
 
 
 def entregables(request):
 
-    return HttpResponse('Vista entregables')
+    return render(request, "AppCoder/entregables.html")
+
+
+def cursoFormulario(request):
+    if request.method == 'POST':
+        miFormulario = CursoFormulario(request.POST)  # Aquí llega toda la información del HTML
+        print(miFormulario)
+
+        if miFormulario.is_valid:  # Si pasó la validación de Django
+            informacion = miFormulario.cleaned_data
+            curso = Curso(nombre=informacion['curso'], camada=informacion['camada'])
+            curso.save()
+            return render(request, "AppCoder/inicio.html")  # Vuelvo al inicio o a donde quieran
+    else:
+        miFormulario = CursoFormulario()  # Formulario vacío para construir el HTML
+    return render(request, "AppCoder/cursoFormulario.html", {"miFormulario":miFormulario})
+
+
+def busquedaCamada(request):
+    return render(request, "AppCoder/busquedaCamada.html")
+
+
+def buscar(request):
+    if request.GET.get('camada'):
+        # respuesta = f"Estoy buscando la camada nro: {request.GET['camada']}"
+        camada = request.GET.get('camada')
+        cursos = Curso.objects.filter(camada__icontains=camada)
+        return render(request, "AppCoder/resultadosBusqueda.html", {"cursos": cursos, "camada":camada})
+    else:
+        respuesta = "No enviaste datos"
+        # No olvidar from django.http import HttpResponse
+        return HttpResponse(respuesta)
+
 # def curso(self):
 #     curso = Curso(nombre="Desarrollo web", camada="19881")
 #     curso.save()
